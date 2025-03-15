77910b17ecfa79f60f3bf55d080284657815d331
Glitch (glitch-hello-website) <none>
2025-03-15 21:59:44 +0000

    👛🐦 Checkpoint

--- test-openai.js
+++
@@ -1,8 +0,0 @@
-const { Configuration, OpenAIApi } = require("openai");
-console.log("keys:", Object.keys(require("openai")));
-
-const configuration = new Configuration({
-  apiKey: "fake-key"
-});
-const openai = new OpenAIApi(configuration);
-console.log("Success!", openai);
